8d86e2e08686480cbc3ab058224d12e2fcfcce73
David Cantoran <davidcantoran36@gmail.com>
2019-11-20 02:21:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -27,7 +27,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(//)
+  if phone.match(/(\d\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}/)
     true 
   else false 
   end 
