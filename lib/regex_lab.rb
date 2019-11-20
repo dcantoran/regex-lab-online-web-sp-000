b6bcf2a6a95e4af963ea9dc09131fdfe4f3e52d9
David Cantoran <davidcantoran36@gmail.com>
2019-11-20 01:53:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,12 +18,12 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\b[A-Z].*[?.!]/)
+  if text.match(/\b[A-Z]\w*[?.!]/)
     true 
-    # binding.pry
   else 
     false 
   end 
+  binding.pry
 end
 
 def valid_phone_number?(phone)
