883ea05c4c35238d58c2a750f58c67df1dc826f5
David Cantoran <davidcantoran36@gmail.com>
2019-11-20 01:09:44 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.scan(/\bun/) && text.scan(/ing/)
-    return text 
-  end 
+  text.scan(/\bun/) && text.scan(/ing/)
     
-  
 end
 
 def words_five_letters_long(text)
