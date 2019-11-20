fff9d1e3991512edc1bb7a195110b583f04ddcaa
David Cantoran <davidcantoran36@gmail.com>
2019-11-20 01:52:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,7 +18,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\b[A-Z][a-z]*[?.!]/)
+  if text.match(/\b[A-Z].*[?.!]/)
     true 
     binding.pry
   else 
