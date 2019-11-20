e5afa73aa1510c2c965c3fbe9f40d059f67d3a45
David Cantoran <davidcantoran36@gmail.com>
2019-11-20 01:08:38 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\bun/) && text.match(/ing/)
+  text.scan(/\bun/) && text.scan(/ing/)
     
   
 end
